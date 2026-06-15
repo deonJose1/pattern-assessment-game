dc3c3849256e34d10236445e9e01d9e19bb1c750
Jose <2513946@cognizant.com>
2026-06-15 18:43:54 +0530

    chore: initial project scaffolding for pattern assessment

---
+++ README.md
@@ -0,0 +1,1 @@
+Master deployment guide
---
+++ assessment-backend/src/main/java/com/cognizant/patternassessment/PatternAssessmentApplication.java
@@ -0,0 +1,6 @@
+// main runner file for the application. This file is responsible for starting the application and initializing any necessary components. It may also contain configuration settings and other important information related to the application.
+package main.java.com.cognizant.patternassessment;
+
+public class PatternAssessmentApplication {
+    
+}
